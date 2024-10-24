7979d629bc8f70b6b7b6128e46a4fa85faf4de13
Catherine Ashton <160202627+Catash37@users.noreply.github.com>
2024-10-24 18:47:24 +1100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,4 +5,5 @@ sudo apt-get install build-essential cmake
 sudo apt-get install libopenblas-dev liblapack-dev
 sudo apt-get install libx11-dev libgtk-3-dev
 sudo apt install cmake
+sudo apt install libdlib-dev
 python -m pip install --no-cache-dir -r requirements.txt
